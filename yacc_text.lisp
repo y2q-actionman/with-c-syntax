dc2af09c882eadb7307d3a70394ab7287b26d738
y2q-actionman <y2q.actionman@gmail.com>
2014-07-24 14:03:41 +0900

    added yacc_text.lisp

---
+++ yacc_text.lisp
@@ -0,0 +1,54 @@
+(in-package :cl-user)
+
+(use-package :yacc)
+
+(defun list-lexer (list)
+  #'(lambda ()
+      (let ((value (pop list)))
+	(cond ((null value)
+	       (values nil nil))
+	      ((symbolp value)
+	       (let ((term (member value '(+ - * / |(| |)|)
+				   :test #'string= :key #'symbol-name)))
+		 (values (if term (car term) 'id) ; returns a symbol of our package.
+			 value)))
+	      ((integerp value)
+	       (values 'int value))
+	      (t
+	       (error "Unexpected value ~S" value))))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun i2p (a b c)
+    "Infix to prefix"
+    (list b a c))
+  
+  (defun k-2-3 (a b c)
+    "Second out of three"
+    (declare (ignore a c))
+    b)
+  )
+
+(define-parser *expression-parser*
+  (:start-symbol expression)
+  (:terminals (int id + - * / |(| |)|))
+  (:precedence ((:left * /) (:left + -)))
+  
+  (expression
+   (expression + expression #'i2p)
+   (expression - expression #'i2p)
+   (expression * expression #'i2p)
+   (expression / expression #'i2p)
+   term)
+  
+  (term
+   id
+   int
+   (- term)
+   (|(| expression |)| #'k-2-3)))
+
+;; (parse-with-lexer (list-lexer '(x * - - 2 + 3 * y)) *expression-parser*)
+;; => (+ (* X (- (- 2))) (* 3 Y))	       
+
+(defun c-expression-tranform (form)
+  (parse-with-lexer (list-lexer form)
+		    *expression-parser*))
