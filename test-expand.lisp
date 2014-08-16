e06ee00ee347d52a0df8aa2ac9d9a5f5ce930985
y2q-actionman <y2q.actionman@gmail.com>
2014-08-17 00:05:47 +0900

    rewrite text-expand

--- test-expand.lisp
+++ test-expand.lisp
@@ -1,69 +1,95 @@
 (in-package :cl-user)
 
-;; (parse-with-lexer (list-lexer '(x * - - 2 + 3 * y)) *expression-parser*)
-;; => (+ (* X (- (- 2))) (* 3 Y))	       
-
-
-#|
-(with-c-syntax ()
-{
-  return 1 + 2 \;
-}
-)
-3
+(defun test-expand-1 ()
+  (with-c-syntax ()
+    {
+    return 1 + 2 \;
+    })
+  ;; => 3
+  )
 
 (defparameter x 0)
-(with-c-syntax () {
-  while \( x < 100 \)
-    x ++ \;
-  return x \;
-}
-)
 
-(defparameter i 0)
-(with-c-syntax ()
-{
-  for \( i = 0 \; i < 100 \; ++ i \)
-  (format t "~A~%" i) \;
-}
-)
+(defun test-expand-2 ()
+  (with-c-syntax ()
+    {
+    while \( x < 100 \)
+      x ++ \;
+    return x \;
+    })
+  ;; => 100
+  )
 
-(defparameter i 0)
-(with-c-syntax ()
-{
-  for \( i = 0 \; i < 100 \; ++ i \) {
-    if \( (oddp i) \)
-      continue \;
-    if \( i == 50 \)
-      break \;
-    (format t "~A~%" i) \;
-  }
-}
-)
+(defun test-expand-3 ()
+  (let ((i 0))
+    (with-c-syntax ()
+      {
+      for \( i = 0 \; i < 100 \; ++ i \)
+      (format t "~A~%" i) \;
+      })
+    ))
 
-(with-c-syntax ()
-{
-  switch \( x \) {
-  case 1 \:
-    (format t "case 1 ni kita~%") \;
-    break \;
-  case 2 \:
-    (format t "case 2 ni kita~%") \;
-    (format t "fall-though~%") \;
-  default \:
-    (format t "default ni kita~%") \;
-  }
-}
-)
+(defun test-expand-4 ()
+  (let ((i 0))
+    (with-c-syntax ()
+      {
+      for \( i = 0 \; i < 100 \; ++ i \) {
+        if \( (oddp i) \)
+          continue \;
+        if \( i == 50 \)
+          break \;
+        (format t "~A~%" i) \;
+      }
+      })
+    ))
+
+(defun test-expand-5 (x)
+  (with-c-syntax ()
+    {
+      switch \( x \) {
+      case 1 \:
+        (format t "case 1 ni kita~%") \;
+        break \;
+      case 2 \:
+        (format t "case 2 ni kita~%") \;
+        (format t "fall-though~%") \;
+      default \:
+        (format t "default ni kita~%") \;
+      }
+    })
+  )
 
 
-(with-c-syntax ()
-{
-  goto a \;
-  a \:
-    return 100 \;
-}
-)
+(defun test-expand-6 ()
+  (with-c-syntax ()
+    {
+    goto a \;
+    a \:
+      return 100 \;
+    })
+  )
+
+(defun test-expand-7 (xxx &aux z)
+  (with-c-syntax (xxx)
+    {
+    z =  & xxx \;
+    (format t "z = ~A~%" z) \;
+
+
+    (format t "xxx = ~A~%" xxx) \;
+
+    * z += 1 \;
+    (format t "xxx = ~A~%" xxx) \;
+
+    * z *= 2 \;
+    (format t "xxx = ~A~%" xxx) \;
+
+    return xxx \;
+    })
+  )
+  
+
+#|
 
 ;; removed dereference temporarylly..
 (with-c-syntax ()
