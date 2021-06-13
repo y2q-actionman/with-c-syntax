7b9e065ee64ce7185a222a1114423d83abb77e8b
y2q-actionman <y2q-actionman@users.noreply.github.com>
2021-06-13 16:14:38 +0900

    Suppressed some "unused tag" warnings in libc-assert test cases

--- test/libc_assert.lisp
+++ test/libc_assert.lisp
@@ -2,22 +2,33 @@
 
 (in-readtable with-c-syntax-readtable)
 
+(defvar *true-for-suppress-warning* t
+  "This value is just `cl:t'.
+ I use this for suppressing 'Unused tag' warnings by `cl:assert' of Allegro CL 10.1.")
+
+(define-condition condition-for-testing-libc-assert (simple-error)
+  ())
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (setf with-c-syntax.libc:NDEBUG nil)
   (test test-assert-ndebug-nil
     #{
     signals (error, |assert| (nil));
-    is (progn (|assert| ("true"), t));
+    is (cl:or (|assert| (*true-for-suppress-warning*), t));
+    ;  // `|assert|' parameter should be evaluated.
+    signals (condition-for-testing-libc-assert,
+             |assert| (error ('condition-for-testing-libc-assert)));
     }#)
   (makunbound 'with-c-syntax.libc:NDEBUG))
-
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (setf with-c-syntax.libc:NDEBUG t)
   (test test-assert-ndebug-t
     #{
-    is (progn (|assert| (nil), t));
-    is (progn (|assert| ("true"), t));
+    is (cl:or (|assert| (nil), t));
+    is (cl:or (|assert| ("true"), t));
+    ;  // `|assert|' parameter should not be evaluated.
+    is (cl:or (|assert| (error ('condition-for-testing-libc-assert)), t));
     }#)
   (makunbound 'with-c-syntax.libc:NDEBUG))
 
@@ -26,7 +37,9 @@
   ;; In top-level, `NDEBUG' is left unbound (at default).
   #{
   signals (error, |assert| (nil));
-  is (progn (|assert| ("true"), t));
+  is (cl:or (|assert| (*true-for-suppress-warning*), t));
+  signals (condition-for-testing-libc-assert,
+           |assert| (error ('condition-for-testing-libc-assert)));
   }#
 
   #+with-c-syntax-test-use-compiler-let
@@ -35,14 +48,17 @@
     (trivial-cltl2:compiler-let ((with-c-syntax.libc:NDEBUG NIL))
       #{
       signals (error, |assert| (nil));
-      is (progn (|assert| ("true"), t));
+      is (cl:or (|assert| (*true-for-suppress-warning*), t));
+      signals (condition-for-testing-libc-assert,
+               |assert| (error ('condition-for-testing-libc-assert)));
       }#)
 
     ;; binding to T -- no error.
     (trivial-cltl2:compiler-let ((with-c-syntax.libc:NDEBUG t))
       #{
-      is (progn (|assert| (nil), t));
-      is (progn (|assert| ("true"), t));
+      is (cl:or (|assert| (nil), t));
+      is (cl:or (|assert| ("true"), t));
+      is (cl:or (|assert| (error ('condition-for-testing-libc-assert)), t));
       }#))
   #-with-c-syntax-test-use-compiler-let
   (warn "Your Lisp does not have 'compiler-let' facility. Skip tests.."))
