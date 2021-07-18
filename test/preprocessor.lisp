38935b63a9da2b8395bc974d2ccde242d970b27b
y2q-actionman <y2q-actionman@users.noreply.github.com>
2021-07-18 14:31:01 +0900

    Added test-pp-6.10.3.5-example-5

--- test/preprocessor.lisp
+++ test/preprocessor.lisp
@@ -702,6 +702,18 @@
      xglue(HIGH, LOW)
      }#)))
 
+(test test-pp-6.10.3.5-example-5
+  (is.wcs.pp.equal
+   #2{
+   int j[] = { 123, 45, 67, 89,
+               10, 11, 12,  };
+   }#
+   #2{
+   #define t(x,y,z) x ## y ## z
+   int j[] = { t(1,2,3), t(,4,5), t(6,,7), t(8,9,),
+              t(10,,), t(,11,), t(,,12), t(,,) };
+   }#))
+
 
 ;;; TODO: add symbol-interning tests
 ;;;  in lexer?
