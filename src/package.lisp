eeb01d3bac02010b44f25b771787267ccdf32211
y2q-actionman <y2q-actionman@users.noreply.github.com>
2021-08-16 00:23:03 +0900

    Uses strings by defpackages for C syntactic definitions, to ensure these character cases

--- src/package.lisp
+++ src/package.lisp
@@ -1,71 +1,72 @@
 (in-package #:cl-user)
 
+;;; Packages for C syntax.
+
 (defpackage #:with-c-syntax.syntax
   (:use) ; Saying I use no packages explicitly. (If omitted, it is implementation-dependent.)
   (:export
    ;; operators
-   #:\,
-   #:= #:*= #:/= #:%= #:+= #:-= #:<<= #:>>= #:&= #:^= #:\|=
-   #:? #:\:
-   #:\|\|
-   #:&&
-   #:\|
-   #:^
-   #:&
-   #:== #:!=
-   #:< #:> #:<= #:>=
-   #:>> #:<<
-   #:+ #:-
-   #:* #:/ #:%
-   #:\( #:\)
-   #:++ #:-- #:|sizeof|
-   #:& #:* #:+ #:- #:~ #:!
-   #:[ #:] #:\. #:->
+   ","
+   "=" "*=" "/=" "%=" "+=" "-=" "<<=" ">>=" "&=" "^=" "|="
+   "?" ":"
+   "||"
+   "&&"
+   "|"
+   "^"
+   "&"
+   "==" "!="
+   "<" ">" "<=" ">="
+   ">>" "<<"
+   "+" "-"
+   "*" "/" "%"
+   "(" ")"
+   "++" "--" "sizeof"
+   "&" "*" "+" "-" "~" "!"
+   "[" "]" "." "->"
    ;; keywords
-   #:\;
-   #:|auto| #:|register| #:|static| #:|extern| #:|typedef|
-   #:|void| #:|char| #:|short| #:|int| #:|long|
-   #:|float| #:|double| #:|signed| #:|unsigned|
-   #:|const| #:|volatile|
-   #:|struct| #:|union|
-   #:|enum|
-   #:|...|
-   #:|case| #:|default|
-   #:{ #:}
-   #:|if| #:|else| #:|switch|
-   #:|while| #:|do| #:|for|
-   #:|goto| #:|continue| #:|break| #:|return|
+   ";"
+   "auto" "register" "static" "extern" "typedef"
+   "void" "char" "short" "int" "long"
+   "float" "double" "signed" "unsigned"
+   "const" "volatile"
+   "struct" "union"
+   "enum"
+   "..."
+   "case" "default"
+   "{" "}"
+   "if" "else" "switch"
+   "while" "do" "for"
+   "goto" "continue" "break" "return"
    ;; extensions
-   #:|__lisp_type| #:|__offsetof|)
+   "__lisp_type" "__offsetof")
   (:documentation
    "Holds symbols denoting C operators and keywords."))
 
 (defpackage #:with-c-syntax.punctuator
   (:use)
   (:import-from #:with-c-syntax.syntax
-                #:[ #:] #:\( #:\) #:{ #:} #:\. #:->
-                #:++ #:-- #:& #:* #:+ #:- #:~ #:!
-                #:/ #:% #:<< #:>> #:< #:> #:<= #:>= #:== #:!= #:^ #:\| #:&& #:\|\|
-                #:? #:\: #:\; #:|...|
-                #:= #:*= #:/= #:%= #:+= #:-= #:<<= #:>>= #:&= #:^= #:\|=
-                #:\,)
+                "[" "]" "(" ")" "{" "}" "." "->"
+                "++" "--" "&" "*" "+" "-" "~" "!"
+                "/" "%" "<<" ">>" "<" ">" "<=" ">=" "==" "!=" "^" "|" "&&" "||"
+                "?" ":" ";" "..."
+                "=" "*=" "/=" "%=" "+=" "-=" "<<=" ">>=" "&=" "^=" "|="
+                ",")
   (:export
-   #:[ #:] #:\( #:\) #:{ #:} #:\. #:->
-   #:++ #:-- #:& #:* #:+ #:- #:~ #:!
-   #:/ #:% #:<< #:>> #:< #:> #:<= #:>= #:== #:!= #:^ #:\| #:&& #:\|\|
-   #:? #:\: #:\; #:|...|
-   #:= #:*= #:/= #:%= #:+= #:-= #:<<= #:>>= #:&= #:^= #:\|=
-   #:\,
+   "[" "]" "(" ")" "{" "}" "." "->"
+   "++" "--" "&" "*" "+" "-" "~" "!"
+   "/" "%" "<<" ">>" "<" ">" "<=" ">=" "==" "!=" "^" "|" "&&" "||"
+   "?" ":" ";" "..."
+   "=" "*=" "/=" "%=" "+=" "-=" "<<=" ">>=" "&=" "^=" "|="
+   ","
    ;; For preprocessor (their home package is just this package.)
-   #:|#| #:|##|
-   #:|<:| #:|:>| #:<% #:%> #:|%:| #:|%:%:|)
+   "#" "##"
+   "<:" ":>" "<%" "%>" "%:" "%:%:")
   (:documentation "C punctuators. Some symbols are from `with-c-syntax.syntax' package."))
 
 (defpackage #:with-c-syntax.preprocessor-directive
   (:use)
-  (:export
-   #:|if| #:|ifdef| #:|ifndef| #:|elif| #:|else| #:|endif| #:|include|
-   #:|define| #:|undef| #:|line| #:|error| #:|pragma|))
+  (:export "if" "ifdef" "ifndef" "elif" "else" "endif" "include"
+           "define" "undef" "line" "error" "pragma"))
 
 (defpackage #:with-c-syntax.preprocess-operator
   (:use)
@@ -77,15 +78,17 @@
 
 (defpackage #:with-c-syntax.predefined-macro
   (:use)
-  (:export #:__DATE__ #:__FILE__ #:__LINE__ #:__STDC__
-           #:__STDC_HOSTED__ #:__STDC_MB_MIGHT_NEQ_WC__
-           #:__STDC_VERSION__ #:__TIME__))
+  (:export "__DATE__" "__FILE__" "__LINE__" "__STDC__"
+           "__STDC_HOSTED__" "__STDC_MB_MIGHT_NEQ_WC__"
+           "__STDC_VERSION__" "__TIME__"))
+
+;;; Package for Lisp
 
 (defpackage #:with-c-syntax.core
   (:use #:cl #:with-c-syntax #:with-c-syntax.syntax)
   (:shadowing-import-from
-   #:cl			      ; These CL symbols has same name with C.
-   #:= #:/=  #:< #:> #:<= #:>=  #:+ #:- #:* #:/  #:++)
+   #:cl              ; Some CL symbols has same name with C operators.
+   #:= #:/= #:< #:> #:<= #:>= #:+ #:- #:* #:/ #:++)
   (:use #:alexandria)
   (:import-from #:yacc
         	#:define-parser
